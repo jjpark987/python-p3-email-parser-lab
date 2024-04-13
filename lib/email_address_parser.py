dabca3838cea95eb40c1986425d13c119bd8dc81
Jason Park <jjpark987@gmail.com>
2024-04-12 18:36:43 -0700

    solution

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,12 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, addresses=str) -> None:
+        self.addresses = addresses
+
+    def parse(self):
+        separated_emails = {email for email in re.split(r'[,\s]+', self.addresses)}
+
+        valid_emails = [email for email in separated_emails if re.match(r'^[\w\.-]+@[\w\.-]+$', email)]
+
+        return sorted(valid_emails)
